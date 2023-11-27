1e381c05bf0e99e770929a5c46d78bfcb39830c3
DeepBlue97 <945048933@qq.com>
2023-11-27 13:49:39 +0800

    yolox

--- src/fnnmodule/model/__init__.py
+++ src/fnnmodule/model/__init__.py
@@ -1,8 +1,10 @@
 from .yolo.yolov3 import YOLOv3
+from .yolo.yolox import YOLOX
 from .movenet.movenet import MoveNet
 
 __all__ = [
     "__version__",
     "YOLOv3",
+    'YOLOX',
     "MoveNet",
 ]
